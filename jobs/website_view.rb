492edd505f1d76091256b90d5daec7359d9c022a
hunii <jjoung@hotmail.co.nz>
2015-12-16 12:04:04 +1300

    change

--- jobs/website_view.rb
+++ jobs/website_view.rb
@@ -6,7 +6,7 @@ require 'open-uri'
 SCHEDULER.every '1d', :first_in => 0 do |job|
 
 	url="http://www.w3schools.com/html/html_iframe.asp"
-	send_event('web_view', url:url)
+	send_event('web_view', src:url)
 
 
 end
