3e27c59a10a99c8a24e4eed4c98d0b822e350d0e
hunii <jjoung@hotmail.co.nz>
2015-12-15 15:14:25 +1300

    change

--- jobs/vacancy_on_off.rb
+++ jobs/vacancy_on_off.rb
@@ -6,7 +6,7 @@ SCHEDULER.every '1d', :first_in => 0 do |job|
     want = [{:name=>"I am looking for job", :body=>"green"}]
 	have = [{:name=>"I am not looking for job", :body=>"red"}]
 
-	message = [{:name=>"At uni until end of 2017", :body=>"orange"}]
+	message = [{:name=>"At uni until 2017", :body=>"orange"}]
 
 	# Looking for job
 	send_event 'want_job', { comments: want } 
